540dd99ea578f216913e08dc8754f6c880863900
djflsdl08 <djflsdl08@naver.com>
2016-09-09 14:50:19 +0900

    Changed the if-statement.

--- Java_2/src/Practice8/Collide.java
+++ Java_2/src/Practice8/Collide.java
@@ -1,19 +1,25 @@
-package Practice8;
+package Chap2;
 import java.util.Scanner;
 
-public class Collide {
+public class Practice8 {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("두 점 (x1,y1), (x2,y2)의 좌표를 입력하시오.>> ");
-		int x1 = scanner.nextInt();
-		int y1 = scanner.nextInt();
-		int x2 = scanner.nextInt();
-		int y2 = scanner.nextInt();
+		int x1,x2,y1,y2;
+		System.out.print("두 점 (x1,y1),(x2,y2)의 좌표를 입력하시오.>> ");
+		x1 = scanner.nextInt();
+		y1 = scanner.nextInt();
+		x2 = scanner.nextInt();
+		y2 = scanner.nextInt();
 		
-		if((x1>50&&x1<100)&&(y1>50&&y1<100)||(x2>50&&x2<100)&&(y2>50&&y2<100))
-			System.out.print("사각형이 겹칩니다.");
-		else System.out.print("사각형이 겹치지 않습니다.");
-		
+		// 두 점 중 적어도 한 점이 사각형 내부에 있는 경우
+		if((x1>=50&&x1<=100)&&(y1>=50&&y1<=100)||(x2>=50&&x2<=100)&&(y2>=50&&y2<=100)){
+			System.out.println("사각형이 겹칩니다.");
+		}
+		// 사각형을 덮을 때
+		else if((x1<50&&y1<50||x1>100&&y1>100)&&(x2<50&&y2<50||x2>100&&y2>100)){
+			System.out.println("사각형이 겹칩니다.");
+		}
+		else System.out.println("사각형이 겹치지 않습니다.");
 		scanner.close();
 	}
 }
